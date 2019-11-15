7e3b682f5c8b4abcf694928d5d32370f0734a6aa
Artem <artem@gratchev.com>
2019-11-15 23:27:01 +0100

    Prepare test for random generatoin

--- color-lines-game.js
+++ color-lines-game.js
@@ -2,11 +2,12 @@ class Game {
     constructor() {
         this.sizeX = 9;
         this.sizeY = 9;
+        this.selectedIndex = -1;
 
         this.board = [];
         for (var y = 0, i = 0; y < this.sizeY; y++) {
             for (var x = 0; x < this.sizeX; x++, i++) {
-                this.board.push(i);
+                this.board.push(0);
             }
         }
     }
@@ -14,4 +15,22 @@ class Game {
     getBoard() {
         return this.board;
     }
+
+    getSelectedIndex() {
+        return this.selectedIndex;
+    }
+
+    selectBall(tileIndex) {
+        this.selectedIndex = tileIndex;
+    }
+
+    countBalls() {
+        var count = 0;
+        for (var i = 0; i < this.board.length; i++) {
+            if (this.board[i] > 0) count++;
+        }
+        return count;
+    }
+
+
 }
--- tests.js
+++ tests.js
@@ -1,4 +1,14 @@
 QUnit.test("Game board initialization", function (assert) {
     var game = new Game();
     assert.strictEqual(game.getBoard().length, 81, "Corect board size");
+    game.getBoard().forEach(tile => {
+        assert.strictEqual(tile, 0, "Tile is empty");
+    });
+    assert.ok(game.getSelectedIndex() < 0, "Nothing selected");
+    assert.strictEqual(game.countBalls(), 0, "No balls in game");
+});
+
+QUnit.test("Generate random indices of free tiles", function (assert) {
+    var game = new Game();
+    game.setTile(0, 1);
 });
