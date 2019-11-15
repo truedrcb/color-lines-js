6070c61c7e8d1d0889f3e0ec5a36105e6877268c
Artem <artem@gratchev.com>
2019-11-15 21:47:18 +0100

    Test of board parameters

--- color-lines.js
+++ color-lines.js
@@ -38,10 +38,16 @@ class PlayField extends React.Component {
 
     render() {
         var rows = [];
+        var i = 0;
         for (var y = 0; y < this.state.sizeY; y++) {
             var cells = [];
             for (var x = 0; x < this.state.sizeX; x++) {
-                cells.push(e('div', {className: 'col'}, 'X'));
+                cells.push(e('div', {className: 'col tile'}, 
+                    e('i', {className: 'fas fa-fw'
+                        + (this.props.board[i] ? ' fa-bowling-ball PUMPKIN' : ' CLOUDS') 
+                        + (i == this.props.selected ? ' fa-spin' : '')
+                        }, '')));
+                i++;
             }
             rows.push(e('div', {className: 'row'}, cells));
         }
@@ -54,5 +60,5 @@ class PlayField extends React.Component {
 
 console.log("test color lines");
 
-ReactDOM.render(e(PlayField), document.querySelector('#color_lines_container'));
+ReactDOM.render(e(PlayField, {board: [1,2,0,3,4], selected: 1}), document.querySelector('#color_lines_container'));
 ReactDOM.render(e(LikeButton), document.getElementById('like_button_container'));
