9430cc072cf87f49eee2c7b690a0626f90edcccb
Artem <artem@gratchev.com>
2019-11-15 21:14:54 +0100

    Build a grid

--- color-lines.js
+++ color-lines.js
@@ -20,7 +20,7 @@ class LikeButton extends React.Component {
                 onClick: () => this.setState({
                     liked: true
                 }),
-                class: "btn btn-primary"
+                className: "btn btn-primary"
             },
             'Like'
         );
@@ -37,7 +37,16 @@ class PlayField extends React.Component {
     }
 
     render() {
+        var rows = [];
+        for (var y = 0; y < this.state.sizeY; y++) {
+            var cells = [];
+            for (var x = 0; x < this.state.sizeX; x++) {
+                cells.push(e('div', {className: 'col'}, 'X'));
+            }
+            rows.push(e('div', {className: 'row'}, cells));
+        }
 
+        return e('div', {className: 'container'}, rows);
     }
 
 }
@@ -45,5 +54,5 @@ class PlayField extends React.Component {
 
 console.log("test color lines");
 
-const domContainer = document.querySelector('#color_lines_container');
-ReactDOM.render(e(LikeButton), domContainer);
+ReactDOM.render(e(PlayField), document.querySelector('#color_lines_container'));
+ReactDOM.render(e(LikeButton), document.getElementById('like_button_container'));
